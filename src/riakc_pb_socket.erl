166a7f6406cb847f243490b04f33e614c3de6da2
Drew <drew.varner@ninefx.com>
2013-12-06 23:33:39 -0500

    Changed the name of the timeout macro in riakc.hrl from DEFAULT_TIMEOUT to DEFAULT_PB_TIMEOUT.
    The name DEFAULT_TIMEOUT is confusing, because riakc.hrl is used in both the riak-erlang-client and riak-erlang-http-client packages. Adding PB to the macro name makes it clear to the user they are changing the default timeout for the protobuf client and not http.

--- src/riakc_pb_socket.erl
+++ src/riakc_pb_socket.erl
@@ -436,7 +436,7 @@ stream_list_buckets(Pid, Options) ->
 stream_list_buckets(Pid, Type, Options) ->
     ServerTimeout =
         case proplists:get_value(timeout, Options, none) of
-            none -> ?DEFAULT_TIMEOUT;
+            none -> ?DEFAULT_PB_TIMEOUT;
             ST -> ST
         end,
     ReqId = mk_reqid(),
@@ -448,7 +448,7 @@ stream_list_buckets(Pid, Type, Options) ->
 legacy_list_buckets(Pid, Options) ->
     ServerTimeout =
         case proplists:get_value(timeout, Options, none) of
-            none -> ?DEFAULT_TIMEOUT;
+            none -> ?DEFAULT_PB_TIMEOUT;
             ST -> ST
         end,
     gen_server:call(Pid, {req, #rpblistbucketsreq{timeout=ServerTimeout},
@@ -507,7 +507,7 @@ stream_list_keys(Pid, Bucket, Timeout) when is_integer(Timeout) ->
 stream_list_keys(Pid, Bucket, Options) ->
     ServerTimeout =
         case proplists:get_value(timeout, Options, none) of
-            none -> ?DEFAULT_TIMEOUT;
+            none -> ?DEFAULT_PB_TIMEOUT;
             ST -> ST
         end,
     {T, B} = maybe_bucket_type(Bucket),
@@ -1110,7 +1110,7 @@ default_timeout(OpTimeout) ->
                 {ok, Timeout} ->
                     Timeout;
                 undefined ->
-                    ?DEFAULT_TIMEOUT
+                    ?DEFAULT_PB_TIMEOUT
             end
     end.
 
