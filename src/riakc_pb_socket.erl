58fd866b2b7c23d5ae0951ee7bf5ad7486d24670
erikleitch <eleitch@basho.com>
2016-01-11 10:04:55 -0800

    Modified riakc_pb_socket.erl to rename ttb message

--- src/riakc_pb_socket.erl
+++ src/riakc_pb_socket.erl
@@ -1965,7 +1965,7 @@ process_response(#request{msg = #tsputreq{}},
                  tsputresp, State) ->
     {reply, ok, State};
 
-process_response(#request{msg = #tsputreqttb{}},
+process_response(#request{msg = #tsttbputreq{}},
                  tsputresp, State) ->
     {reply, ok, State};
 
