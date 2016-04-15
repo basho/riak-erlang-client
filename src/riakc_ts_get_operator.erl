4954a99b9c9fda37133fead88d712e898385df68
Brett Hazen <brett@basho.com>
2016-04-14 22:00:52 -0600

    Handle tsgetresp case of emtpy rows

--- src/riakc_ts_get_operator.erl
+++ src/riakc_ts_get_operator.erl
@@ -48,6 +48,8 @@ deserialize({error, Message}) ->
     {error, Message};
 deserialize(tsgetresp) ->
     {ok, {[], []}};
+deserialize({tsgetresp, {_, _, []}}) ->
+    {[], []};
 deserialize({tsgetresp, {ColumnNames, _ColumnTypes, Rows}}) ->
     {ok, {ColumnNames, Rows}};
 deserialize(#tsgetresp{columns = C, rows = R}) ->
