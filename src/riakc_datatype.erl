78cb23553ff9fb7b28a6c2b552ad77b5fadb69f8
Bryan Hunt <bryan.hunt@bet365.com>
2016-07-27 16:01:57 +0100

    try out lukes requests

--- src/riakc_datatype.erl
+++ src/riakc_datatype.erl
@@ -34,7 +34,7 @@
 -endif.
 
 
--define(MODULES, [riakc_set, riakc_counter, riakc_flag, riakc_register, riakc_map]).
+-define(MODULES, [riakc_set, riakc_gset, riakc_counter, riakc_flag, riakc_register, riakc_map]).
 
 -export([module_for_type/1,
          module_for_term/1]).
@@ -82,6 +82,7 @@
 %% type.
 -spec module_for_type(Type::atom()) -> module().
 module_for_type(set)      -> riakc_set;
+module_for_type(gset)     -> riakc_gset;
 module_for_type(counter)  -> riakc_counter;
 module_for_type(flag)     -> riakc_flag;
 module_for_type(register) -> riakc_register;
