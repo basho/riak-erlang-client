2913f9573d2decb481fa4b56712e714b417be651
paulhunt <paulhunt@uk365office.co.uk>
2020-05-19 12:27:03 +0100

    develop-3.0-intelligent_client - Changed node watcher subscribe call to include update fun. Also changed node watcher unsubscribe call to use new riak_pb request.

--- src/riakc_pb_socket.erl
+++ src/riakc_pb_socket.erl
@@ -79,7 +79,7 @@
          get_ring/1, get_ring/2,
          get_default_bucket_props/1, get_default_bucket_props/2,
          get_nodes/1, get_nodes/2,
-         node_watcher_subscribe/1, node_watcher_subscribe/2,
+         node_watcher_subscribe/2, node_watcher_subscribe/3,
          node_watcher_unsubscribe/1, node_watcher_unsubscribe/2]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -1352,29 +1352,27 @@ get_nodes(Pid) ->
 get_nodes(Pid, Timeout) ->
     call_infinity(Pid, {req, rpbgetnodesreq, Timeout}).
 
--spec node_watcher_subscribe(Pid :: pid()) ->
+-spec node_watcher_subscribe(Pid :: pid(), AdminPid :: function()) ->
     ok.
-node_watcher_subscribe(Pid) ->
-    Req = riak_pb_kv_codec:encode_node_watcher_subscribe_req(Pid),
+node_watcher_subscribe(Pid, UpdateFun) ->
+    Req = riak_pb_kv_codec:encode_node_watcher_subscribe_req(UpdateFun),
     call_infinity(Pid, {req, Req, default_timeout(node_watcher_subscribe_timeout)}).
 
--spec node_watcher_subscribe(Pid :: pid(), Timeout :: non_neg_integer()) ->
+-spec node_watcher_subscribe(Pid :: pid(), AdminPid :: function(), Timeout :: non_neg_integer()) ->
     ok.
-node_watcher_subscribe(Pid, Timeout) ->
-    Req = riak_pb_kv_codec:encode_node_watcher_subscribe_req(Pid),
+node_watcher_subscribe(Pid, UpdateFun, Timeout) ->
+    Req = riak_pb_kv_codec:encode_node_watcher_subscribe_req(UpdateFun),
     call_infinity(Pid, {req, Req, Timeout}).
 
 -spec node_watcher_unsubscribe(Pid :: pid()) ->
     ok.
 node_watcher_unsubscribe(Pid) ->
-    Req = riak_pb_kv_codec:encode_node_watcher_unsubscribe_req(Pid),
-    call_infinity(Pid, {req, Req, default_timeout(node_watcher_unsubscribe_timeout)}).
+    call_infinity(Pid, {req, rpbnodewatcherunsubscribereq, default_timeout(node_watcher_unsubscribe_timeout)}).
 
 -spec node_watcher_unsubscribe(Pid :: pid(), Timeout :: non_neg_integer()) ->
     ok.
 node_watcher_unsubscribe(Pid, Timeout) ->
-    Req = riak_pb_kv_codec:encode_node_watcher_unsubscribe_req(Pid),
-    call_infinity(Pid, {req, Req, Timeout}).
+    call_infinity(Pid, {req, rpbnodewatcherunsubscribereq, Timeout}).
 
 %% ====================================================================
 %% gen_server callbacks
@@ -2128,12 +2126,12 @@ process_response(#request{msg = rpbgetnodesreq}, Result, State) ->
     Nodes = riak_pb_kv_codec:decode_nodes(Result),
     {reply, {ok, Nodes}, State};
 process_response(undefined, Reply, State) when erlang:is_record(Reply, rpbnodewatcherupdate) ->
-    Nodes = riak_pb_kv_codec:decode_node_watcher_update(Reply),
-    riakc_ic_information:update_nodes(Nodes),
+    {Nodes, UpdateFun} = riak_pb_kv_codec:decode_node_watcher_update(Reply),
+    UpdateFun(Nodes),
     {reply, ok, State};
 process_response(#request{msg = #rpbnodewatchersubscribereq{}}, rpbnodewatchersubscriberesp, State) ->
     {reply, ok, State};
-process_response(#request{msg = #rpbnodewatcherunsubscribereq{}}, rpbnodewatcherunsubscriberesp, State) ->
+process_response(#request{msg = rpbnodewatcherunsubscribereq}, _Result, State) ->
     {reply, ok, State};
 process_response(Request, Reply, State) ->
     %% Unknown request/response combo
